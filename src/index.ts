9fb4a7729027d785618c124c282ada9be632c69d
driback00 <driback00@outlook.com>
2025-02-18 21:33:32 +0700

    Remove unused middleware imports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
 import { Hono } from 'hono';
-import { every } from 'hono/combine';
-import { cors } from 'hono/cors';
-import { secureHeaders } from 'hono/secure-headers';
 import { HlsController } from './modules/hls/hls.controller';
 import { MediaController } from './modules/media/media.controller';
 
 const app = new Hono();
-
-app.use(
-  '*',
-  every(
-    cors({
-      origin: '*',
-      allowMethods: ['GET'],
-      allowHeaders: ['Content-Type'],
-    }),
-    secureHeaders({
-      strictTransportSecurity: 'max-age=63072000; includeSubDomains; preload',
-      xFrameOptions: 'DENY',
-      xXssProtection: '1',
-    })
-  )
-);
 
 app.get('/', (c) => c.text('Hono!'));
 app.get('/media', ...MediaController);
